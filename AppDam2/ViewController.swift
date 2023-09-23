c0dde97c4078ad4de0cf33ec9debd74891cc8073
mysticBel <dnxbel@gmail.com>
2023-09-23 01:55:52 -0500

    sesion3 p1 - btnCalcular da resultedo

--- AppDam2/ViewController.swift
+++ AppDam2/ViewController.swift
@@ -21,5 +21,19 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func btnCalcular(_ sender: UIButton) {
+        //variables
+        var n1, n2, resu: Int
+        //var num:String?
+        
+        //entrada
+          //num = txtNumero1.text!  significa que hay algo
+          //print(num)  se imprime un nro del textfield en el cmd
+        // condicional ,  se lee la caja 1, sino sale 0 , el segundo significa que es 0 (optional)
+        n1 = Int(txtNumero1.text ?? "0") ?? 0
+        n2 = Int(txtNumero2.text ?? "0") ?? 0
+        resu = n1 + n2
+        lblResultado.text = " Resultado : "+String(resu)
+    }
 }
 
