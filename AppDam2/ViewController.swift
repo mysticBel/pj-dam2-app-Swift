498e57f28c9aaf1974154c397b2df03d2334580b
mysticBel <dnxbel@gmail.com>
2023-09-22 23:45:17 -0500

    vista ViewController y  atributos en Main

--- AppDam2/ViewController.swift
+++ AppDam2/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //atributes (entradas y salidas)
+    @IBOutlet weak var txtNumero1: UITextField!
+    @IBOutlet weak var txtNumero2: UITextField!
+    @IBOutlet weak var lblResultado: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
